1a9b9a65acbd6e6b3d88eef8786492648823b79a
XianghuWang-287 <wangxianghu1019@126.com>
2024-08-08 14:02:17 -0700

    Update the polish_subgraph_hybrid_MST

--- bin/scripts/CAST.py
+++ bin/scripts/CAST.py
@@ -104,7 +104,7 @@ def polish_subgraph_hybrid_MST(G):
     mst = nx.maximum_spanning_tree(G_no_trans_align_score,weight='Cosine')
 
     if set(mst.nodes())!=set(G.nodes()):
-        nodes_to_add = set(mst.nodes()) - set(G.nodes())
+        nodes_to_add = set(G.nodes()) - set(mst.nodes())
         mst.add_nodes_from(nodes_to_add)
 
     # Check if MST is connected
